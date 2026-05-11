630b57c9ba0b4914d1ac2be9843553f49210dadf
vince <vince@elevatedigital.nl>
2026-05-11 10:35:44 +0200

    cleaned up code and updated package-lock

--- nodes/AdCallsHook/AdCallsHook.node.ts
+++ nodes/AdCallsHook/AdCallsHook.node.ts
@@ -9,24 +9,8 @@ const config: WebhookNodeConfig<StandardCallEventBefore> = {
 	icon: 'file:../../lib/adcalls.svg',
 	defaultPath: '',
 	categories: ['AdCalls'],
-
 	validatePayload: isAdCallsPayload,
 	transformPayload: transformAdCallsPayload,
-
-	additionalProperties: [
-		// {
-		// 	displayName: 'Event Types',
-		// 	name: 'eventTypes',
-		// 	type: 'multiOptions',
-		// 	options: [
-		// 		{ name: 'Call Started', value: 'call_started' },
-		// 		{ name: 'Call Ended', value: 'call_ended' },
-		// 		{ name: 'Call Missed', value: 'call_missed' },
-		// 	],
-		// 	default: ['call_started', 'call_ended', 'call_missed'],
-		// 	description: 'Filter which event types trigger the workflow',
-		// },
-	],
 };
 
 export class AdCallsHook extends BaseWebhookNode<StandardCallEventBefore> {
