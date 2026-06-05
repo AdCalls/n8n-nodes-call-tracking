bf74d1a0183cfb1b48f317687d4a899c567b4333
vince <vincedejong@hotmail.com>
2026-06-05 12:56:53 +0200

    increased package version - changed n8n category - removed interal docker url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,1 @@
-// export const API_BASE_URL = 'https://api.adcalls.nl';
-export const API_BASE_URL = 'http://call-tracking-api-adcalls.docker.localhost';
+export const API_BASE_URL = 'https://api.adcalls.nl';
